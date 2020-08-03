74049b96777c92ee7678642765d45ab939bf37ba
tyamo <tyamo.dev.0125@gmail.com>
2020-08-04 01:15:39 +0900

    first push

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="app">
+    <div class="myName">
+      I'm RK
+      Nice to meet you
+    </div>  
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+}
+</script>
+
+<style>
+
+.myName {
+  color: red;
+  font-size: 48px;
+  font-weight: 500;
+}
+</style>
