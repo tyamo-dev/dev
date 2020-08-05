cc75132759830d1e69f59b974c1be601c23472d3
tyamo <tyamo.dev.0125@gmail.com>
2020-08-06 00:22:37 +0900

    making category botton now

---
+++ src/components/LangSelector.vue
@@ -0,0 +1,37 @@
+<template>
+	<div class="selector">
+		<div class="selector-text">
+			{{ lang || 'none' }}
+		</div>
+	</div>
+</template>
+
+<script>
+export default {
+	name: 'LangSelector',
+	props: {
+		lang: {
+			type: String,
+			default: null
+		}
+	}
+}
+</script>
+
+<style lang="less" scoped>
+@import '../variables.less';
+.selector {
+	margin-top: 20px;
+	width: 70px;
+	height: 70px;
+	background: @dark-green;
+	opacity: 1.3;
+	color: white;
+	border-radius: 50%;
+	text-align: center;
+
+	.selector-text {
+		padding-top: 22px;
+	}
+}
+</style>
---
+++ src/views/Contents.vue
@@ -0,0 +1,74 @@
+<template>
+	<div>
+		<div class="selectors">
+			<div 
+				v-for=" lang in languageFirst"
+				:key="lang"
+				class="selector"
+				>
+				<LangSelector 
+					:lang="lang"
+					class="inline"
+				/>
+				
+			</div>
+		</div>
+		<div class="selectors second">
+			<div 
+				v-for=" lang in languageSecond"
+				:key="lang"
+				class="selector"
+				>
+				<LangSelector 
+					:lang="lang"
+					class="inline"
+				/>
+			</div>
+		</div>
+		</div>
+</template>
+
+<script>
+import LangSelector from '../components/LangSelector.vue';
+
+export default {
+	name: 'Contests',
+	components: {
+		LangSelector,
+	},
+	data() {
+		return {
+			languageFirst: [ 'HTML', 'css', 'python', 'js'],
+			languageSecond: ['java' ,'C' ,'go' ,'R'],
+		}
+	}
+}
+</script>
+
+<style lang="less">
+
+.selectors {
+	display: flex;
+
+	.selector {
+		margin: 0 auto;
+	}
+
+	.selector:hover {
+		opacity: 0.7;
+	}
+
+	.inline {
+		display: inline-block;
+	}
+
+}
+
+.second {
+	margin-top: 3%;
+}
+
+.not-leftmost {
+	margin-left: 20px;
+}
+</style>
